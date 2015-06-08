481add61ad073cef3d1841052f85d9f355278c29
sandy <sandeep.s1605@gmail.com>
2015-06-08 18:09:30 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Main application init file.
+ * This will spin up an HTTP SERVER which will listen on connections on port 4040
+ * @author      ritesh
+ * @version     1.0
+ */
+console.log('hi');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  router = require('./router'),
+  // config = require('config'), 
+  errorHandler = require('./middlewares/ErrorHandler'),
+  responser = require('./middlewares/responser');
+ 
+var port = 4040;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(router());
+app.use(responser());
+app.use(errorHandler());
+app.listen(port);
+console.log('Application listening on port ' + port);

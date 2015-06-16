b5defc653a8a8c03d3e10ffe80ead01d1e83fac6
sandy <sandeep.s1605@gmail.com>
2015-06-16 10:59:43 +0530

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,11 +2,14 @@
 
 var userService = require('../services/UserService');
 
+
+
 exports.register = function(req, res, next) {
 	var entity = req.body;
 	if(!entity.firstName || !entity.email) {
-		next({code: 400, message: 'First name or email address cannot be null or empty'});
-	}
+    next({code: 400, message: 'First name or email address cannot be null or empty'});
+  }
+  
   userService.register(entity, function(err, response) {
   	if(err) {
   		return next(err);
@@ -19,7 +22,32 @@ exports.register = function(req, res, next) {
   });
 };
 
+
 exports.login = function(req, res, next) {
+  console.log('login called');
+  var entity = req.body;
+  if(!entity.email || !entity.password) {
+    next({code: 400, message: 'Email or pasword is missing'});
+  }
+userService.login(entity, function(err, response){
+  if(err){
+    return next(err);
+  }
+  if(response){
+  req.data={
+      statusCode: 200,
+      content: 'Hi, ' + response.firstName + ' you are welcome here'
+    };
+  }
+  else{
+  req.data={
+      statusCode: 404,
+      content: 'Login failed for ' + entity.email + ', incorrect email or password'
+    };
+    
+  }
+  next();
+});    
 
 };
 
